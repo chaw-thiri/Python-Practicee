56decccb3e7d52d7d75f58f2e3e56fb88d01b50d
Chaw Thiri San <113085742+chaw-thiri@users.noreply.github.com>
2023-12-25 02:12:38 +0900

    Update studentFile.py

--- StudentRecord/studentFile.py
+++ StudentRecord/studentFile.py
@@ -1,11 +1,11 @@
 class StudentRecord:
     # store a student's data
-    def __init__(self):
-        self._id = 0
-        self._firstName = None
-        self._lastName = None
-        self._classID = 0
-        self._GPA = 0.0
+    def __init__(self, id=0, firstName=None, lastName=None, classID=0, gpa=0.0):
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.classID = classID
+        self.GPA = gpa
 
 
 class StudentFileReader:
@@ -26,22 +26,45 @@ class StudentFileReader:
 
     # Get individual record
     def fetchRecord(self):
-        line = self._file.readline()  # read the first line
-        if line == "": return None  # if the file is empty return None
-        # fill the data as an object and return it
+        line = self._file.readline().strip()  # Read the first line and remove leading/trailing whitespace
+        if not line:  # Check if the line is empty (end of file)
+            return None
+
+        # Fill the data as an object and return it
         student = StudentRecord()
-        student._id = int(line)
-        student._firstName = self._file.readline().rstrip()
-        student._lastName = self._file.readline().rstrip()
-        student._classID = int(self._file.readline())
-        student._GPA = float(self._file.readline())
+        student.id = int(line)
+        student.firstName = self._file.readline().rstrip()
+        student.lastName = self._file.readline().rstrip()
+        student.classID = int(self._file.readline())
+        student.GPA = float(self._file.readline())
         return student
 
     # get all the records
     def fetchAll(self):
         records = []
         student = self.fetchRecord()
-        while student != None:
+        while student is not None:
             records.append(student)
             student = self.fetchRecord()
         return records
+
+
+class StudentFileWriter:
+    # for write record in the file
+    def __init__(self, source):
+        self._source = source
+        self._file = None
+
+    def open(self):
+        self._file = open(self._source, "a")
+
+    def close(self):
+        self._file.close()
+        self._file = None
+
+    def writeRecord(self, student):
+        self._file.write(str(student.id) + "\n")
+        self._file.write(student.firstName + "\n")
+        self._file.write(student.lastName + "\n")
+        self._file.write(str(student.classID) + "\n")
+        self._file.write(str(student.GPA) + "\n")
